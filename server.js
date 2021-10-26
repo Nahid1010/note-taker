f557a065b2db98288d870c4535fc6b15773c58f6
Nahid <Nahidmahmud14@yahoo.com>
2021-10-26 18:30:36 -0400

    done

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 // initial port
-const PORT = 3000;
+const PORT =  process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
